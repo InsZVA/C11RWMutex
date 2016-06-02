8884b7d6b1269e62e2b3fba3291f3abe07b4f947
InsZVA <inszva@126.com>
2016-06-03 00:15:35 +0800

    fix bugs

--- rwmutex.h
+++ rwmutex.h
@@ -1,44 +1,36 @@
-#include <mutex>
-#include <condition_variable>
-
 class RWMutex {
-	private:
-		
-		std::mutex rm ,wm;
-		std::unique_lock<std::mutex> rmtx, wmtx;
-		std::condition_variable cond;
-		int rd_cnt = 0;	//wait for read
-		bool wr_cnt = false;	//wait for write
-	public:
-		RWMutex() : rmtx(rm, std::defer_lock), wmtx(wm, std::defer_lock) {}
-		void rLock() {
-			rmtx.lock();
-			++rd_cnt;
-			while (wr_cnt)
-				cond.wait(rmtx);
-			rmtx.unlock();
-		}
+private:
+	
+	std::mutex wm, rmm;
+	std::unique_lock<std::mutex> rmtx;
+	std::condition_variable cond;
+	int rd_cnt = 0;	//wait for read
+	bool wr_cnt = false;	//wait for write
+public:
+	RWMutex() : rmtx(rmm, std::defer_lock) {}
+	void rLock() {
+		while (wr_cnt)
+			cond.wait(rmtx);
+	}
 
-		void rUnLock() {
-			rmtx.lock();
-			--rd_cnt;
-			if (rd_cnt == 0)
-				cond.notify_all();
-			rmtx.unlock();
-		}
-
-		void wLock() {
- 			wmtx.lock();
-			while (wr_cnt || rd_cnt > 0)
-				cond.wait(wmtx);
-			wr_cnt = true;
-			wmtx.unlock();
-		}
-
-		void wUnlock() {
-			wmtx.lock();
-			wr_cnt = false;
+	void rUnLock() {
+		if (!wr_cnt)
 			cond.notify_all();
-			wmtx.unlock();
-		}
-	};
+	}
+
+	void wLock() {
+		wm.lock();
+		while (wr_cnt)
+			cond.wait(rmtx);
+		wr_cnt = true;
+		wm.unlock();
+	}
+
+	void wUnlock() {
+		wm.lock();
+		wr_cnt = false;
+		if (!wr_cnt)
+			cond.notify_all();
+		wm.unlock();
+	}
+};
